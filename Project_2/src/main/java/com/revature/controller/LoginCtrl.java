f79b38b9efdb5b2fcd2f836d7a32c24dbba5894f
unknown <aric.rhodes95@gmail.com>
2018-10-20 17:21:22 -0500

    restful login

--- Project_2/src/main/java/com/revature/controller/LoginCtrl.java
+++ Project_2/src/main/java/com/revature/controller/LoginCtrl.java
@@ -10,67 +10,46 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.revature.pojo.User;
 import com.revature.service.AuthService;
 
 @Controller
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class LoginCtrl {
-	
+
 	@Autowired
 	private AuthService authService;
-	
-	@RequestMapping(value="/login", method=RequestMethod.GET)
-	public String loginGet(HttpSession sess){
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String loginGet(HttpSession sess) {
 		System.out.println("inside login");
-		if (sess.getAttribute("user") != null){
+		if (sess.getAttribute("user") != null) {
 			return "home";
 		}
 		return "login";
 	}
-	
-	//Testing alternative method
-  @RequestMapping(value="/login", method=RequestMethod.POST)
-	public String loginPost(@RequestBody User user, BindingResult bindingResult, ModelMap modelMap, HttpSession sess){
-	  		
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json")
+	public @ResponseBody User loginPost(@RequestBody User user, BindingResult bindingResult, ModelMap modelMap,
+			HttpSession sess) {
+
 		User authUser = authService.validate(user);
-		
-		if (bindingResult.hasErrors()){
+
+		if (bindingResult.hasErrors()) {
 			modelMap.addAttribute("errorMessage", bindingResult.getAllErrors().get(0).getDefaultMessage());
-			return "login";
+			return null;
 		}
-		
-		if(authUser != null){
+
+		if (authUser != null) {
 			sess.setAttribute("user", authUser);
-			return "home";
+			return authUser;
 		}
-		
+
 		modelMap.addAttribute("errorMessage", "Username or password incorrect");
-		
-		return "login";
+
+		return null;
 	}
-	
-/*	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public String loginPost( String username, String password){
-		
-		System.out.println("Inside Login Control Post");
-		
-		User user = new User();
-		
-		user.setUsername(username);
-		user.setPassword(password);
-		
-		User authUser = authService.validate(user);
-		
-		System.out.println("after AuthUser");
-		
-		if(authUser != null){ 
-			return "home";
-		}
-		
-		return "login";
-	}
-*/
 
 }
